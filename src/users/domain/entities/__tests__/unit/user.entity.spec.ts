07f9f7293c010a2a8931a19f2e43e8621def4161
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-18 05:11:17 -0300

    UserEntity - constructor unit tests

---
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,22 @@
+import { UserProps } from './../../user.entity';
+import { faker } from '@faker-js/faker';
+import { UserEntity } from '../../user.entity';
+describe('UserEntity unit tests', () => {
+  let props: UserProps;
+  let sut: UserEntity;
+  beforeEach(() => {
+    props = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    sut = new UserEntity(props);
+  });
+  it('Constructor method', () => {
+    expect(sut.props.name).toEqual(props.name);
+    expect(sut.props.email).toEqual(props.email);
+    expect(sut.props.password).toEqual(props.password);
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
+});
