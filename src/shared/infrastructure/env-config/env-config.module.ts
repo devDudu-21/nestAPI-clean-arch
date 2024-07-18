efe0e9bddc25e41f8ca908f3c76c05003734c5c1
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-18 04:39:18 -0300

    EnvConfigService unit tests

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -1,10 +1,6 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { EnvConfigService } from './env-config.service';
-import {
-  ConfigModule,
-  ConfigModuleOptions,
-  ConfigService,
-} from '@nestjs/config';
+import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
 import { join } from 'node:path';
 
 @Module({
@@ -14,7 +10,9 @@ export class EnvConfigModule extends ConfigModule {
   static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
     return super.forRoot({
       ...options,
-      envFilePath: [join(__dirname, `../../../.env.${process.env.NODE_ENV}`)],
+      envFilePath: [
+        join(__dirname, `../../../../.env.${process.env.NODE_ENV}`),
+      ],
     });
   }
 }
