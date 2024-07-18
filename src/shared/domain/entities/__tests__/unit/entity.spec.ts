96d4add600e85ee08ba524bbd59162a2004537ac
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-18 06:07:43 -0300

    Abstract class entity - unit tests

---
+++ src/shared/domain/entities/__tests__/unit/entity.spec.ts
@@ -0,0 +1,39 @@
+import { validate as validateUUID } from 'uuid';
+import { Entity } from '../../entity';
+
+type StubProps = {
+  prop1: string;
+  prop2: number;
+};
+
+class StubEntity extends Entity<StubProps> {}
+describe('Entity unit tests', () => {
+  it('should se props and id', () => {
+    const props = { prop1: 'value1', prop2: 15 };
+    const entity = new StubEntity(props);
+
+    expect(entity.props).toStrictEqual(props);
+    expect(entity._id).not.toBeNull();
+    expect(validateUUID(entity._id)).toBeTruthy();
+  });
+
+  it('should accept a valid uuid', () => {
+    const props = { prop1: 'value1', prop2: 15 };
+    const id = '98fa40ea-9cf2-4e0d-a150-4cf12d379ba3';
+    const entity = new StubEntity(props, id);
+
+    expect(validateUUID(entity._id)).toBeTruthy();
+    expect(entity._id).toEqual(id);
+  });
+
+  it('should convert a entity to a json', () => {
+    const props = { prop1: 'value1', prop2: 15 };
+    const id = '98fa40ea-9cf2-4e0d-a150-4cf12d379ba3';
+    const entity = new StubEntity(props, id);
+
+    expect(entity.toJson()).toStrictEqual({
+      id,
+      ...props,
+    });
+  });
+});
