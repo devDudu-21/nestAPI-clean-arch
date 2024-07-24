357d0765a462a9c38bd038a54476f26a584ad257
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-24 15:55:38 -0300

    InMemoryRepository unit tests - findById method

--- src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -1,5 +1,6 @@
 import { Entity } from '@/shared/domain/entities/entity';
 import { InMemoryRepository } from '../../in-memory.repository';
+import { NotFoundError } from '@/shared/domain/errors/not-found-error';
 
 type StubEntityProps = {
   name: string;
@@ -20,7 +21,19 @@ describe('InMemoryRepository unit tests', () => {
   it('should inserts a new entity', async () => {
     const entity = new StubEntity({ name: 'test name', price: 10 });
     await sut.insert(entity);
-
     expect(entity.toJson()).toStrictEqual(sut.items[0].toJson());
+  });
+
+  it('should throw error when entity is not found', async () => {
+    await expect(sut.findById('fake id')).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    );
+  });
+
+  it('should find a entity by id', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 10 });
+    await sut.insert(entity);
+    const result = await sut.findById(entity._id);
+    expect(entity.toJson()).toStrictEqual(result.toJson());
   });
 });
