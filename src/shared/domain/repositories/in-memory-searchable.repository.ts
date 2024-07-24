0a647897b5d2a0e7f22ea45b39fae74fd905e2a3
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-24 20:29:15 -0300

    Adding SearchableRepositoryInterface

---
+++ src/shared/domain/repositories/in-memory-searchable.repository.ts
@@ -0,0 +1,12 @@
+import { SearchableRepositoryInterface } from './searchable-repository-contract';
+import { Entity } from '../entities/entity';
+import { InMemoryRepository } from './in-memory.repository';
+
+export abstract class InMemorySearchableRepository<E extends Entity>
+  extends InMemoryRepository<E>
+  implements SearchableRepositoryInterface<E, any, any>
+{
+  search(props: any): Promise<any> {
+    throw new Error('Method not implemented.');
+  }
+}
--- src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -1,11 +1,11 @@
 import { ConflictError } from '@/shared/domain/errors/conflict-error';
 import { NotFoundError } from '@/shared/domain/errors/not-found-error';
-import { InMemoryRepository } from '@/shared/domain/repositories/in-memory.repository';
+import { InMemorySearchableRepository } from '@/shared/domain/repositories/in-memory-searchable.repository';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { UserRepository } from '@/users/domain/repositories/user.repository';
 
 export class UserInMemoryRepository
-  extends InMemoryRepository<UserEntity>
+  extends InMemorySearchableRepository<UserEntity>
   implements UserRepository
 {
   async findByEmail(email: string): Promise<UserEntity> {
