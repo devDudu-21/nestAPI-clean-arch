2c9c6d1b9b8fcd4f8557ef77364d3011efb86996
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-24 15:58:15 -0300

    InMemoryRepository unit tests - findAll method

--- src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -36,4 +36,11 @@ describe('InMemoryRepository unit tests', () => {
     const result = await sut.findById(entity._id);
     expect(entity.toJson()).toStrictEqual(result.toJson());
   });
+
+  it('should return all entities', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 10 });
+    await sut.insert(entity);
+    const result = await sut.findAll();
+    expect([entity]).toStrictEqual(result);
+  });
 });
