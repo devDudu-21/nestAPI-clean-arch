2fb19de6bcfc716c544984bde2f296b8146c4227
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-25 20:12:33 -0300

    feat: Refactor UseCase classes to implement shared UseCase interface
    This commit refactors the `GetUserUseCase` and `SignupUseCase` classes to implement the shared `UseCase` interface from the `use-case.ts` file. By implementing this interface, the codebase becomes more modular and follows a consistent pattern for executing use cases. This change enhances the maintainability and readability of the code.

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -3,6 +3,7 @@ import { UserRepository } from '@/users/domain/repositories/user.repository';
 import { BadRequestError } from '../errors/bad-request-error';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { UserOutput } from '../dto/user-output';
+import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
 
 export namespace SignupUseCase {
   export type Input = {
@@ -13,7 +14,7 @@ export namespace SignupUseCase {
 
   export type Output = UserOutput;
 
-  export class UseCase {
+  export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(
       private readonly userRepository: UserRepository.Repository,
       private hashProvider: HashProvider,
