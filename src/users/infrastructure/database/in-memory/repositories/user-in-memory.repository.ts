a2aa963d47f2f791aa6bdb78e6a1210b47f22d4e
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-25 01:32:58 -0300

    feat: Add sortableFields property to UserInMemoryRepository
    This commit adds a `sortableFields` property to the `UserInMemoryRepository` class, allowing for sorting of user entities by name and creation date. This change enhances the search functionality and provides more flexibility in retrieving and displaying user data.

--- src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -1,13 +1,16 @@
 import { ConflictError } from '@/shared/domain/errors/conflict-error';
 import { NotFoundError } from '@/shared/domain/errors/not-found-error';
 import { InMemorySearchableRepository } from '@/shared/domain/repositories/in-memory-searchable.repository';
+import { SortDirection } from '@/shared/domain/repositories/searchable-repository-contract';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { UserRepository } from '@/users/domain/repositories/user.repository';
 
 export class UserInMemoryRepository
   extends InMemorySearchableRepository<UserEntity>
-  implements UserRepository
+  implements UserRepository.Repository
 {
+  sortableFields: string[] = ['name', 'createdAt'];
+
   async findByEmail(email: string): Promise<UserEntity> {
     const entity = this.items.find(item => item.email === email);
     if (!entity) {
@@ -21,5 +24,27 @@ export class UserInMemoryRepository
     if (entity) {
       throw new ConflictError(`Email ${email} already exists`);
     }
+  }
+
+  protected async applyFilter(
+    items: UserEntity[],
+    filter: UserRepository.Filter,
+  ): Promise<UserEntity[]> {
+    if (!filter) {
+      return items;
+    }
+    return items.filter(item => {
+      return item.props.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  }
+
+  protected async applySort(
+    items: UserEntity[],
+    sort: string | null,
+    sortDir: SortDirection | null,
+  ): Promise<UserEntity[]> {
+    return !sort
+      ? super.applySort(items, 'createdAt', 'desc')
+      : super.applySort(items, sort, sortDir);
   }
 }
