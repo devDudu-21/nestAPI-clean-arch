4cdf929f1fa8234f08619cb3dbda8ece48a5e4bf
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-25 17:58:19 -0300

    Refactor hashProvider interface to use PascalCase for HashProvider

--- src/shared/application/providers/hash-provider.ts
+++ src/shared/application/providers/hash-provider.ts
@@ -1,4 +1,4 @@
-export interface hashProvider {
+export interface HashProvider {
   generateHash(payload: string): Promise<string>;
   compareHash(payload: string, hash: string): Promise<boolean>;
 }
