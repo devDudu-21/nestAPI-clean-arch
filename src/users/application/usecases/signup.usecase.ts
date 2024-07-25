78b57660eb3386a213f0dc947304c6e471fb5630
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-25 20:14:20 -0300

    Refactor import paths for BadRequestError in signup.usecase.ts and signup.usecase.spec.ts

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -1,6 +1,6 @@
 import { HashProvider } from '@/shared/application/providers/hash-provider';
 import { UserRepository } from '@/users/domain/repositories/user.repository';
-import { BadRequestError } from '../errors/bad-request-error';
+import { BadRequestError } from '../../../shared/application/errors/bad-request-error';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { UserOutput } from '../dto/user-output';
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
