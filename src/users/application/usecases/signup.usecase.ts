89f1f5d88c5869e6406e95f0b4fd7432b5cb085c
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-25 17:09:33 -0300

    feat: Add SignupUseCase to handle user sign up functionality
    This commit adds the `SignupUseCase` class to handle the user sign up functionality. The `SignupUseCase` class includes an `execute` method that takes in user input and performs the necessary validations and operations to create a new user entity. This addition enhances the application's functionality by providing a way for users to sign up and be added to the system.

---
+++ src/users/application/usecases/signup.usecase.ts
@@ -0,0 +1,36 @@
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { BadRequestError } from '../errors/bad-request-error';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+
+export namespace SignupUseCase {
+  export type Input = {
+    name: string;
+    email: string;
+    password: string;
+  };
+
+  export type Output = {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+  };
+
+  export class UseCase {
+    constructor(private readonly userRepository: UserRepository.Repository) {}
+    async execute(input: Input): Promise<Output> {
+      const { email, name, password } = input;
+      if (!email || !name || !password) {
+        throw new BadRequestError('Missing required fields');
+      }
+
+      await this.userRepository.emailExists(email);
+
+      const entity = new UserEntity(input);
+
+      await this.userRepository.insert(entity);
+      return entity.toJSON();
+    }
+  }
+}
