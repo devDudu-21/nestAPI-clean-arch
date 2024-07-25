2dffc965d9c490d75668cc3a79677d41efbde472
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-24 21:02:13 -0300

    Adding SearchParams class

--- src/shared/domain/repositories/searchable-repository-contract.ts
+++ src/shared/domain/repositories/searchable-repository-contract.ts
@@ -1,10 +1,95 @@
 import { Entity } from '../entities/entity';
 import { RepositoryInterface } from './repository-contracts';
 
+export type SortDirection = 'asc' | 'desc';
+
+export type SearchProps<Filter = string> = {
+  page?: number;
+  perPage?: number;
+  sort?: string | null;
+  sortDir?: SortDirection | null;
+  filter?: Filter | null;
+};
+
+export class SearchParams {
+  protected _page: number;
+  protected _perPage: 15;
+  protected _sort: string | null;
+  protected _sortDir: SortDirection | null;
+  protected _filter: string | null;
+
+  constructor(props: SearchProps) {
+    this._page = props.page;
+    this._perPage = props.perPage;
+    this._sort = props.sort;
+    this._sortDir = props.sortDir;
+    this._filter = props.filter;
+  }
+
+  get page() {
+    return this._page;
+  }
+
+  private set page(value: number) {
+    let _page = +value;
+    if (Number.isNaN(_page) || _page <= 0 || parseInt(_page as any) !== _page) {
+      _page = 1;
+    }
+    this._page = _page;
+  }
+
+  get perPage() {
+    return this._perPage;
+  }
+
+  private set perPage(value: number) {
+    let _perPage = +value;
+    if (
+      Number.isNaN(_perPage) ||
+      _perPage <= 0 ||
+      parseInt(_perPage as any) !== _perPage
+    ) {
+      _perPage = 1;
+    }
+    this._perPage = _perPage;
+  }
+
+  get sort() {
+    return this._sort;
+  }
+
+  private set sort(value: string | null) {
+    this._sort =
+      value === null || value === undefined || value === '' ? null : `${value}`;
+  }
+
+  get sortDir() {
+    return this._sortDir;
+  }
+
+  private set sortDir(value: SortDirection | null) {
+    if (!this.sort) {
+      this._sortDir = null;
+      return;
+    }
+    const dir = `${value}`.toLowerCase();
+    this._sortDir = dir !== `asc` && dir !== 'desc' ? 'desc' : dir;
+  }
+
+  get filter() {
+    return this._filter;
+  }
+
+  private set filter(value: string | null) {
+    this._filter =
+      value === null || value === undefined || value === '' ? null : `${value}`;
+  }
+}
+
 export interface SearchableRepositoryInterface<
   E extends Entity,
   SearchInput,
   SearchOutput,
 > extends RepositoryInterface<E> {
-  search(props: SearchInput): Promise<SearchOutput>;
+  search(props: SearchParams): Promise<SearchOutput>;
 }
