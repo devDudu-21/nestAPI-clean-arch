9578c60b2b83f49c86e5ad08a4db71f19654f31b
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-25 01:32:43 -0300

    feat: Refactor UserRepository to use generic types
    This commit refactors the `UserRepository` to use generic types for the `Filter`, `SearchParams`, and `SearchResult` interfaces. By introducing generic types, we improve the flexibility and reusability of the repository, allowing for different types of filters and search parameters to be used. This change aligns with the recent code changes in the `SearchableRepositoryInterface` and ensures consistency throughout the codebase.

--- src/users/domain/repositories/user.repository.ts
+++ src/users/domain/repositories/user.repository.ts
@@ -1,8 +1,24 @@
 import { UserEntity } from '../entities/user.entity';
-import { SearchableRepositoryInterface } from '@/shared/domain/repositories/searchable-repository-contract';
+import {
+  SearchParams as DefaultSearchParams,
+  SearchResult as DefaultSearchResult,
+  SearchableRepositoryInterface,
+} from '@/shared/domain/repositories/searchable-repository-contract';
 
-export interface UserRepository
-  extends SearchableRepositoryInterface<UserEntity, any, any> {
-  findByEmail(email: string): Promise<UserEntity>;
-  emailExists(email: string): Promise<void>;
+export namespace UserRepository {
+  export type Filter = string;
+
+  export class SearchParams extends DefaultSearchParams<Filter> {}
+
+  export class SearchResult extends DefaultSearchResult<UserEntity, Filter> {}
+  export interface Repository
+    extends SearchableRepositoryInterface<
+      UserEntity,
+      Filter,
+      SearchParams,
+      SearchResult
+    > {
+    findByEmail(email: string): Promise<UserEntity>;
+    emailExists(email: string): Promise<void>;
+  }
 }
