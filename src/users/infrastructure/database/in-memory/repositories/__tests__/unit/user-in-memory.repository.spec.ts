00c6c34b201a0bc17348ead1e2c35f111325d866
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-25 02:07:39 -0300

    feat: Add applyFilter and applySort units tests of UserInMemoryRepository

--- src/users/infrastructure/database/in-memory/repositories/__tests__/unit/user-in-memory.repository.spec.ts
+++ src/users/infrastructure/database/in-memory/repositories/__tests__/unit/user-in-memory.repository.spec.ts
@@ -58,4 +58,58 @@ describe('UserInMemoryRepository unit tests', () => {
     expect(spyFilter).toHaveBeenCalled();
     expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
   });
+
+  it('should filter name field using filter param', async () => {
+    const items = [
+      new UserEntity(UserDataBuilder({ name: 'Test' })),
+      new UserEntity(UserDataBuilder({ name: 'TEST' })),
+      new UserEntity(UserDataBuilder({ name: 'fake' })),
+    ];
+    const spyFilter = jest.spyOn(items, 'filter');
+    const itemsFiltered = await sut['applyFilter'](items, 'TEST');
+    expect(spyFilter).toHaveBeenCalled();
+    expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
+  });
+
+  it('should sort by createdAt when sort param is null', async () => {
+    const createdAt = new Date();
+    const items = [
+      new UserEntity(UserDataBuilder({ name: 'Test', createdAt })),
+      new UserEntity(
+        UserDataBuilder({
+          name: 'TEST',
+          createdAt: new Date(createdAt.getTime() + 1),
+        }),
+      ),
+      new UserEntity(
+        UserDataBuilder({
+          name: 'fake',
+          createdAt: new Date(createdAt.getTime() + 2),
+        }),
+      ),
+    ];
+    const itemsSorted = await sut['applySort'](items, null, null);
+    expect(itemsSorted).toStrictEqual([items[2], items[1], items[0]]);
+  });
+
+  it('should sort by name field', async () => {
+    const items = [
+      new UserEntity(UserDataBuilder({ name: 'c' })),
+      new UserEntity(
+        UserDataBuilder({
+          name: 'd',
+        }),
+      ),
+      new UserEntity(
+        UserDataBuilder({
+          name: 'a',
+        }),
+      ),
+    ];
+    let itemsSorted = await sut['applySort'](items, 'name', 'asc');
+    expect(itemsSorted).toStrictEqual([items[2], items[0], items[1]]);
+
+    itemsSorted = await sut['applySort'](items, 'name', null);
+    expect(itemsSorted).toStrictEqual([items[1], items[0], items[2]]);
+  });
 });
