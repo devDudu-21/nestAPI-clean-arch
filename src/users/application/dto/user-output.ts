95010b701b4d34c818e9636dd4384055e1091c3c
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-25 20:38:45 -0300

    feat: Add PaginationOutputMapper for mapping search results to pagination output
    This commit introduces the `PaginationOutputMapper` class, which is responsible for mapping search results to the `PaginationOutput` type. It takes in an array of items and a `SearchResult` object and returns a `PaginationOutput` object with the mapped data. This addition enhances the codebase by providing a standardized way to transform search results into pagination output throughout the application.

--- src/users/application/dto/user-output.ts
+++ src/users/application/dto/user-output.ts
@@ -1,3 +1,5 @@
+import { UserEntity } from '@/users/domain/entities/user.entity';
+
 export type UserOutput = {
   id: string;
   name: string;
@@ -5,3 +7,9 @@ export type UserOutput = {
   password: string;
   createdAt: Date;
 };
+
+export class UserOutputMapper {
+  static toOutput(entity: UserEntity): UserOutput {
+    return entity.toJSON();
+  }
+}
--- src/users/application/usecases/listusers.usecase.ts
+++ src/users/application/usecases/listusers.usecase.ts
@@ -1,18 +1,30 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository';
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
 import { SearchInput } from '@/shared/application/dto/search-input';
+import {
+  PaginationOutput,
+  PaginationOutputMapper,
+} from '@/shared/application/dto/pagination-output';
+import { UserOutput, UserOutputMapper } from '../dto/user-output';
 
 export namespace ListUsersUseCase {
   export type Input = SearchInput;
 
-  export type Output = void;
+  export type Output = PaginationOutput<UserOutput>;
 
   export class UseCase implements DefaultUseCase<Input, Output> {
     constructor(private readonly userRepository: UserRepository.Repository) {}
     async execute(input: Input): Promise<Output> {
       const params = new UserRepository.SearchParams(input);
       const searchResult = await this.userRepository.search(params);
-      return;
+      return this.toOutput(searchResult);
+    }
+
+    private toOutput(searchResult: UserRepository.SearchResult): Output {
+      const items = searchResult.items.map(item => {
+        return UserOutputMapper.toOutput(item);
+      });
+      return PaginationOutputMapper.toOutput(items, searchResult);
     }
   }
 }
