d32de731c78c9f726db5fa039a192794827f4f54
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-25 17:41:58 -0300

    feat: Add generateHash, compareHash and generateSalt unit tests of BcryptjsHashProvider

---
+++ src/users/infrastructure/providers/hash-provider/__tests__/unit/bcryptjs-hash.provider.spec.ts
@@ -0,0 +1,38 @@
+import { BcryptjsHashProvider } from '../../bcryptjs-hash.provider';
+
+describe('BcryptjsHashProvider unit tests', () => {
+  let sut: BcryptjsHashProvider;
+
+  beforeEach(() => {
+    sut = new BcryptjsHashProvider();
+  });
+
+  it('should return encrypted password', async () => {
+    const password = 'TestPassword123';
+    const hash = await sut.generateHash(password);
+    const isValid = await sut.compareHash(password, hash);
+    expect(isValid).toBeTruthy();
+  });
+
+  it('should return false on invalid password and hash comparison', async () => {
+    const password = 'TestPassword123';
+    const hash = await sut.generateHash(password);
+    const result = await sut.compareHash('fake', hash);
+    expect(result).toBeFalsy();
+  });
+
+  it('should return true on valid password and hash comparison', async () => {
+    const password = 'TestPassword123';
+    const hash = await sut.generateHash(password);
+    const result = await sut.compareHash(password, hash);
+    expect(result).toBeTruthy();
+  });
+
+  it('should generate salt correctly', async () => {
+    const spy = jest.spyOn(BcryptjsHashProvider, 'genSalt');
+    const salt = await BcryptjsHashProvider.genSalt();
+    expect(salt).toBeDefined();
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
+  });
+});
