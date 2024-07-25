62fc4505b1e554cfb60d7b8f5696e5aca3588123
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-25 01:32:38 -0300

    feat: Refactor SearchParams class to support generic filter type
    The commit refactors the `SearchParams` class to support a generic `Filter` type, allowing for more flexibility in defining search parameters. This change ensures that the `filter` property can accept different types of filters, improving the usability and extensibility of the `SearchableRepositoryInterface`.

--- src/shared/domain/repositories/searchable-repository-contract.ts
+++ src/shared/domain/repositories/searchable-repository-contract.ts
@@ -21,14 +21,14 @@ export type SearchResultProps<E extends Entity, Filter> = {
   filter: Filter | null;
 };
 
-export class SearchParams {
+export class SearchParams<Filter = string> {
   protected _page: number;
   protected _perPage: number = 15;
   protected _sort: string | null;
   protected _sortDir: SortDirection | null;
-  protected _filter: string | null;
+  protected _filter: Filter | null;
 
-  constructor(props: SearchProps = {}) {
+  constructor(props: SearchProps<Filter> = {}) {
     this.page = props.page;
     this.perPage = props.perPage;
     this.sort = props.sort;
@@ -86,13 +86,15 @@ export class SearchParams {
     this._sortDir = dir !== `asc` && dir !== 'desc' ? 'desc' : dir;
   }
 
-  get filter() {
+  get filter(): Filter | null {
     return this._filter;
   }
 
-  private set filter(value: string | null) {
+  private set filter(value: Filter | null) {
     this._filter =
-      value === null || value === undefined || value === '' ? null : `${value}`;
+      value === null || value === undefined || value === ''
+        ? null
+        : (`${value}` as any);
   }
 }
 
@@ -134,9 +136,9 @@ export class SearchResult<E extends Entity, Filter = string> {
 export interface SearchableRepositoryInterface<
   E extends Entity,
   Filter = string,
-  SearchInput = SearchParams,
+  SearchInput = SearchParams<Filter>,
   SearchOutput = SearchResult<E, Filter>,
 > extends RepositoryInterface<E> {
   sortableFields: string[];
-  search(props: SearchParams): Promise<SearchOutput>;
+  search(props: SearchInput): Promise<SearchOutput>;
 }
