570acf9e5ca49366e36c9f604f23c637aaed02ef
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-27 22:50:21 -0300

    feat: Add UpdateUserUseCase for updating user information

---
+++ src/users/application/usecases/updateuser.usecase.ts
@@ -0,0 +1,26 @@
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { UserOutput, UserOutputMapper } from '../dto/user-output';
+import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case';
+import { BadRequestError } from '@/shared/application/errors/bad-request-error';
+
+export namespace UpdateUserUseCase {
+  export type Input = {
+    id: string;
+    name: string;
+  };
+
+  export type Output = UserOutput;
+
+  export class UseCase implements DefaultUseCase<Input, Output> {
+    constructor(private readonly userRepository: UserRepository.Repository) {}
+    async execute(input: Input): Promise<Output> {
+      if (!input.name) {
+        throw new BadRequestError('Name not provided');
+      }
+      const entity = await this.userRepository.findById(input.id);
+      entity.update(input.name);
+      await this.userRepository.update(entity);
+      return UserOutputMapper.toOutput(entity);
+    }
+  }
+}
