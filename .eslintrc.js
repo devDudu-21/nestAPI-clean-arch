0284652d4d9ce71a63f89268a54b2b91561ff7f8
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-29 23:51:51 -0300

    refactor: Remove unused import in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,8 +22,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-namespace': 'off',
   },
-}
+};
