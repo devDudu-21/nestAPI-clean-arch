bf0c8334b249cdd0acdb7d350cfc87949385ebcb
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-31 07:35:50 -0300

    refactor: Update UsersController unit tests to use dependency injection
    This commit refactors the unit tests for the `UsersController` class in the `users.controller.spec.ts` file to use dependency injection for the various use cases. It replaces the previous direct instantiation of the use cases with injected instances, improving the modularity and testability of the tests.

--- src/users/infrastructure/__tests__/unit/users.controller.spec.ts
+++ src/users/infrastructure/__tests__/unit/users.controller.spec.ts
@@ -1,6 +1,15 @@
+import { UpdatePasswordDto } from './../../dto/update-password.dto';
 import { SignupUseCase } from '@/users/application/usecases/signup.usecase';
 import { UsersController } from '../../users.controller';
 import { UserOutput } from '@/users/application/dto/user-output';
+import { SigninUseCase } from '@/users/application/usecases/signin.usecase';
+import { SigninDto } from '../../dto/signin.dto';
+import { UpdateUserDto } from '../../dto/update-user.dto';
+import { SignupDto } from '../../dto/signup.dto';
+import { UpdateUserUseCase } from '@/users/application/usecases/updateuser.usecase';
+import { UpdatePasswordUseCase } from '@/users/application/usecases/updatepassword.usecase';
+import { GetUserUseCase } from '@/users/application/usecases/getuser.usecase';
+import { ListUsersUseCase } from '@/users/application/usecases/listusers.usecase';
 
 describe('UsersController unit tests', () => {
   let sut: UsersController;
@@ -29,7 +38,7 @@ describe('UsersController unit tests', () => {
       execute: jest.fn().mockReturnValue(Promise.resolve(output)),
     };
     sut['signupUseCase'] = mockSignupUseCase as any;
-    const input: SignupUseCase.Input = {
+    const input: SignupDto = {
       name: props.name,
       email: props.email,
       password: props.password,
@@ -37,5 +46,101 @@ describe('UsersController unit tests', () => {
     const result = await sut.create(input);
     expect(output).toMatchObject(result);
     expect(mockSignupUseCase.execute).toHaveBeenCalledWith(input);
+  });
+
+  it('should be able to authenticate a user', async () => {
+    const output: SigninUseCase.Output = props;
+    const mockSigninUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    sut['signinUseCase'] = mockSigninUseCase as any;
+    const input: SigninDto = {
+      email: props.email,
+      password: props.password,
+    };
+    const result = await sut.login(input);
+    expect(output).toMatchObject(result);
+    expect(mockSigninUseCase.execute).toHaveBeenCalledWith(input);
+  });
+
+  it('should be able to update a user', async () => {
+    const output: UpdateUserUseCase.Output = props;
+    const mockUpdateUserUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    sut['updateUserUseCase'] = mockUpdateUserUseCase as any;
+    const input: UpdateUserDto = {
+      name: 'other name',
+    };
+    const result = await sut.update(id, input);
+    expect(output).toMatchObject(result);
+    expect(mockUpdateUserUseCase.execute).toHaveBeenCalledWith({
+      id,
+      ...input,
+    });
+  });
+
+  it('should be able to update a password', async () => {
+    const output: UpdatePasswordUseCase.Output = props;
+    const mockUpdatePasswordUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    sut['updatePasswordUseCase'] = mockUpdatePasswordUseCase as any;
+    const input: UpdatePasswordDto = {
+      password: props.password,
+      oldPassword: '1234567',
+    };
+    const result = await sut.updatePassword(id, input);
+    expect(output).toMatchObject(result);
+    expect(mockUpdatePasswordUseCase.execute).toHaveBeenCalledWith({
+      id,
+      ...input,
+    });
+  });
+
+  it('should be able to delete a user', async () => {
+    const output: void = undefined;
+    const mockDeleteUserUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    sut['deleteUserUseCase'] = mockDeleteUserUseCase as any;
+
+    const result: void = await sut.remove(id);
+    expect(output).toStrictEqual(result);
+    expect(mockDeleteUserUseCase.execute).toHaveBeenCalledWith({
+      id,
+    });
+  });
+
+  it('should gets a user', async () => {
+    const output: GetUserUseCase.Output = props;
+    const mockGetUserUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    sut['getUserUseCase'] = mockGetUserUseCase as any;
+    const result = await sut.findOne(id);
+    expect(output).toMatchObject(result);
+    expect(mockGetUserUseCase.execute).toHaveBeenCalledWith({ id });
+  });
+
+  it('should list all users', async () => {
+    const output: ListUsersUseCase.Output = {
+      items: [props],
+      total: 1,
+      currentPage: 1,
+      lastPage: 1,
+      perPage: 1,
+    };
+    const mockListUsersUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    const searchParams = {
+      page: 1,
+      perPage: 1,
+    };
+    sut['listUsersUseCase'] = mockListUsersUseCase as any;
+    const result = await sut.search(searchParams);
+    expect(output).toMatchObject(result);
+    expect(mockListUsersUseCase.execute).toHaveBeenCalledWith(searchParams);
   });
 });
