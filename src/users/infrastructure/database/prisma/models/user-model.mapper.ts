173b251abf5e60fc46527e1fe0094aa3f7b30293
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-31 09:32:16 -0300

    feat: Refactor UserPrismaRepository to use UserModelMapper
    The commit refactors the `UserPrismaRepository` class in the `user-prisma.repository.ts` file to use the `UserModelMapper` for mapping the Prisma model to a user entity. This change improves the separation of concerns and enhances the maintainability of the code.

---
+++ src/users/infrastructure/database/prisma/models/user-model.mapper.ts
@@ -0,0 +1,22 @@
+import { ValidationError } from '@/shared/domain/errors/validation-error';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { User } from '@prisma/client';
+
+export class UserModelMapper {
+  static toEntity(model: User) {
+    const data = {
+      name: model.name,
+      email: model.email,
+      password: model.password,
+      createdAt: model.createdAt,
+    };
+
+    try {
+      return new UserEntity(data, model.id);
+    } catch {
+      throw new ValidationError(
+        'An entity could not be created from the model',
+      );
+    }
+  }
+}
--- src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
+++ src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
@@ -1,6 +1,8 @@
+import { NotFoundError } from '@/shared/domain/errors/not-found-error';
 import { PrismaService } from '@/shared/infrastructure/database/prisma/prisma.service';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { UserModelMapper } from '../models/user-model.mapper';
 
 export class UserPrismaRepository implements UserRepository.Repository {
   sortableFields: string[];
@@ -25,8 +27,8 @@ export class UserPrismaRepository implements UserRepository.Repository {
     throw new Error('Method not implemented.');
   }
 
-  findById(id: string): Promise<UserEntity> {
-    throw new Error('Method not implemented.');
+  async findById(id: string): Promise<UserEntity> {
+    return this._get(id);
   }
 
   findAll(): Promise<UserEntity[]> {
@@ -39,5 +41,16 @@ export class UserPrismaRepository implements UserRepository.Repository {
 
   delete(id: string): Promise<void> {
     throw new Error('Method not implemented.');
+  }
+
+  protected async _get(id: string): Promise<UserEntity> {
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: { id },
+      });
+      return UserModelMapper.toEntity(user);
+    } catch {
+      throw new NotFoundError(`UserModel not found using ID ${id}`);
+    }
   }
 }
