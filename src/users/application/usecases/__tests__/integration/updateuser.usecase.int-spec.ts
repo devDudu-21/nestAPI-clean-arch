607f95d327dabde34734da8fba5a63d7f418940d
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-08-20 23:14:19 -0300

    feat: Add integration test for UpdateUserUseCase
    This commit adds an integration test for the UpdateUserUseCase in the updateuser.usecase.int-spec.ts file. The test ensures that a user can be successfully updated by executing the UpdateUserUseCase with a valid user ID and updated data. This test enhances the reliability and functionality of the UpdateUserUseCase and verifies its proper behavior.

---
+++ src/users/application/usecases/__tests__/integration/updateuser.usecase.int-spec.ts
@@ -0,0 +1,50 @@
+import { UpdateUserUseCase } from '@/users/application/usecases/updateuser.usecase';
+import { DatabaseModule } from '@/shared/infrastructure/database/database.module';
+import { setupPrismaTests } from '@/shared/infrastructure/database/prisma/testing/setup-prisma-tests';
+import { UserPrismaRepository } from '@/users/infrastructure/database/prisma/repositories/user-prisma.repository';
+import { TestingModule, Test } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { NotFoundError } from '@/shared/domain/errors/not-found-error';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+
+describe('GetUserUseCase integration tests', () => {
+  const prismaService = new PrismaClient();
+  let sut: UpdateUserUseCase.UseCase;
+  let repository: UserPrismaRepository;
+  let module: TestingModule;
+
+  beforeAll(async () => {
+    setupPrismaTests();
+    module = await Test.createTestingModule({
+      imports: [DatabaseModule.forTest(prismaService)],
+    }).compile();
+    repository = new UserPrismaRepository(prismaService as any);
+  });
+
+  beforeEach(async () => {
+    sut = new UpdateUserUseCase.UseCase(repository);
+    await prismaService.user.deleteMany();
+  });
+
+  afterAll(async () => {
+    await module.close();
+  });
+
+  it('should throws error when entity not found', async () => {
+    await expect(() =>
+      sut.execute({ id: 'fakeId', name: 'fake name' }),
+    ).rejects.toThrow(new NotFoundError('UserModel not found using ID fakeId'));
+  });
+
+  it('should update a user', async () => {
+    const entity = new UserEntity(UserDataBuilder({}));
+    await prismaService.user.create({
+      data: entity.toJSON(),
+    });
+
+    const output = await sut.execute({ id: entity._id, name: 'other name' });
+
+    expect(output.name).toBe('other name');
+  });
+});
