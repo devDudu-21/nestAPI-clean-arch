1ae144880b0a18b1ad994c07b2a6da92cfc8ec92
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-08-20 23:10:14 -0300

    feat: Add integration test for ListUsersUseCase
    This commit adds an integration test for the ListUsersUseCase in the listusers.usecase.int-spec.ts file. The test ensures that the ListUsersUseCase returns the users ordered by createdAt and can also handle filtering, sorting, and pagination. This test enhances the reliability and functionality of the ListUsersUseCase and verifies its proper behavior.

---
+++ src/users/application/usecases/__tests__/integration/listusers.usecase.int-spec.ts
@@ -0,0 +1,111 @@
+import { DatabaseModule } from '@/shared/infrastructure/database/database.module';
+import { setupPrismaTests } from '@/shared/infrastructure/database/prisma/testing/setup-prisma-tests';
+import { UserPrismaRepository } from '@/users/infrastructure/database/prisma/repositories/user-prisma.repository';
+import { TestingModule, Test } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+import { ListUsersUseCase } from '../../listusers.usecase';
+
+describe('ListUsersUseCase integration tests', () => {
+  const prismaService = new PrismaClient();
+  let sut: ListUsersUseCase.UseCase;
+  let repository: UserPrismaRepository;
+  let module: TestingModule;
+
+  beforeAll(async () => {
+    setupPrismaTests();
+    module = await Test.createTestingModule({
+      imports: [DatabaseModule.forTest(prismaService)],
+    }).compile();
+    repository = new UserPrismaRepository(prismaService as any);
+  });
+
+  beforeEach(async () => {
+    sut = new ListUsersUseCase.UseCase(repository);
+    await prismaService.user.deleteMany();
+  });
+
+  afterAll(async () => {
+    await module.close();
+  });
+
+  it('should returns the users ordered by createdAt', async () => {
+    const createdAt = new Date();
+    const entities: UserEntity[] = [];
+    const arrange = Array(3).fill(UserDataBuilder({}));
+    arrange.forEach((element, index) => {
+      entities.push(
+        new UserEntity({
+          ...element,
+          email: `test${index}@mail.com`,
+          createdAt: new Date(createdAt.getTime() + index),
+        }),
+      );
+    });
+
+    await prismaService.user.createMany({
+      data: entities.map(item => item.toJSON()),
+    });
+
+    const output = await sut.execute({});
+
+    expect(output).toStrictEqual({
+      items: entities.reverse().map(item => item.toJSON()),
+      total: 3,
+      currentPage: 1,
+      perPage: 15,
+      lastPage: 1,
+    });
+  });
+
+  it('should returns output using filter, sort and paginate', async () => {
+    const createdAt = new Date();
+    const entities: UserEntity[] = [];
+    const arrange = ['test', 'a', 'TEST', 'b', 'TeSt'];
+    arrange.forEach((element, index) => {
+      entities.push(
+        new UserEntity({
+          ...UserDataBuilder({ name: element }),
+          createdAt: new Date(createdAt.getTime() + index),
+        }),
+      );
+    });
+
+    await prismaService.user.createMany({
+      data: entities.map(entity => entity.toJSON()),
+    });
+
+    let output = await sut.execute({
+      page: 1,
+      perPage: 2,
+      sort: 'name',
+      sortDir: 'asc',
+      filter: 'TEST',
+    });
+
+    expect(output).toMatchObject({
+      items: [entities[0].toJSON(), entities[4].toJSON()],
+      total: 3,
+      currentPage: 1,
+      perPage: 2,
+      lastPage: 2,
+    });
+
+    output = await sut.execute({
+      page: 2,
+      perPage: 2,
+      sort: 'name',
+      sortDir: 'asc',
+      filter: 'TEST',
+    });
+
+    expect(output).toMatchObject({
+      items: [entities[2].toJSON()],
+      total: 3,
+      currentPage: 2,
+      perPage: 2,
+      lastPage: 2,
+    });
+  });
+});
