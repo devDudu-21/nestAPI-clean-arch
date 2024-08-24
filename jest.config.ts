c4299e71db94ee81ebcc24e34bd0fa852071a045
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-08-24 18:25:09 -0300

    fix: Jest test regex to match only unit tests

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ export default {
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/',
   }),
-  testRegex: '.*\\..*spec\\.ts$',
+  testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
