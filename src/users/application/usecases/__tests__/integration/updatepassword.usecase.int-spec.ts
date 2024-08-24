24ed313905b6f5dcdbb1606eb53c9016718b7c2b
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-08-24 18:25:33 -0300

    fix: UpdatePasswordUseCase typo to throws error when entity not found

--- src/users/application/usecases/__tests__/integration/updatepassword.usecase.int-spec.ts
+++ src/users/application/usecases/__tests__/integration/updatepassword.usecase.int-spec.ts
@@ -36,7 +36,7 @@ describe('UpdatePasswordUseCase integration tests', () => {
     await module.close();
   });
 
-  it('should throws error when a entity found', async () => {
+  it('should throws error when a entity not found', async () => {
     const entity = new UserEntity(UserDataBuilder({}));
 
     await expect(() =>
