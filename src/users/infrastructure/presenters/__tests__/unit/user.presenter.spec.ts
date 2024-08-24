0bd472cfe4f5a76c173f10fd89403cc61a38545b
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-08-24 18:25:46 -0300

    feat: Add instanceToPlain function to UserPresenter unit test

--- src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -1,5 +1,6 @@
 import { UserOutput } from '@/users/application/dto/user-output';
 import { UserPresenter } from '../../user.presenter';
+import { instanceToPlain } from 'class-transformer';
 
 describe('UserPresenter unit tests', () => {
   const createdAt = new Date();
@@ -11,13 +12,27 @@ describe('UserPresenter unit tests', () => {
     createdAt,
   };
 
-  it('constructor', () => {
+  let sut: UserPresenter;
+
+  beforeEach(() => {
+    sut = new UserPresenter(props);
+  });
+
+  describe('constructor', () => {
     it('should be defined', () => {
-      const sut = new UserPresenter(props);
       expect(sut.id).toEqual(props.id);
       expect(sut.name).toEqual(props.name);
       expect(sut.email).toEqual(props.email);
       expect(sut.createdAt).toEqual(props.createdAt);
+    });
+  });
+  it('should presenter data', () => {
+    const output = instanceToPlain(sut);
+    expect(output).toStrictEqual({
+      id: 'a86e1d1c-276d-41c7-8ef9-fc1f4d10bf48',
+      name: 'Test name',
+      email: 'a@a.com',
+      createdAt: createdAt.toISOString(),
     });
   });
 });
