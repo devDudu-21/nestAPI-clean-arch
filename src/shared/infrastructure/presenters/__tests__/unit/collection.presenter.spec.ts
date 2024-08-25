c1e94ca38758d06b45f648106935e2913e24cebe
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-08-24 21:38:52 -0300

    feat: Add unit test for CollectionPresenter

---
+++ src/shared/infrastructure/presenters/__tests__/unit/collection.presenter.spec.ts
@@ -0,0 +1,42 @@
+import { CollectionPresenter } from '../../collection.presenter';
+import { PaginationPresenter } from '../../pagination.presenter';
+import { instanceToPlain } from 'class-transformer';
+
+class StubCollectionPresenter extends CollectionPresenter {
+  data = [1, 2, 3];
+}
+
+describe('CollectionPresenter unit tests', () => {
+  let sut: StubCollectionPresenter;
+
+  beforeEach(() => {
+    sut = new StubCollectionPresenter({
+      currentPage: 1,
+      perPage: 2,
+      lastPage: 2,
+      total: 4,
+    });
+  });
+  describe('constructor', () => {
+    it('should set values', () => {
+      expect(sut['paginationPresenter']).toBeInstanceOf(PaginationPresenter);
+      expect(sut['paginationPresenter'].currentPage).toBe(1);
+      expect(sut['paginationPresenter'].perPage).toBe(2);
+      expect(sut['paginationPresenter'].lastPage).toBe(2);
+      expect(sut['paginationPresenter'].total).toBe(4);
+    });
+
+    it('should presented data', () => {
+      const output = instanceToPlain(sut);
+      expect(output).toStrictEqual({
+        data: [1, 2, 3],
+        meta: {
+          currentPage: 1,
+          perPage: 2,
+          lastPage: 2,
+          total: 4,
+        },
+      });
+    });
+  });
+});
