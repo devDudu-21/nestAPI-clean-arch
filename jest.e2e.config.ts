4b83a51ee562f85f8d77f106ae587b3b52657c9f
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-08-24 23:01:02 -0300

    feat: Update Jest configuration to ignore test files in specific directories

---
+++ jest.e2e.config.ts
@@ -0,0 +1,17 @@
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  testRegex: '.*\\.e2e-spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/build/'],
+};
