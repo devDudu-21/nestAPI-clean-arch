720a16f0b33e8c4bb08f3c7dea057a04bc0c3ad8
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-08-26 20:03:49 -0300

    feat: Apply global config for validation and serialization
    This commit applies a global configuration for validation and serialization in the application. It adds a validation pipe to handle incoming requests, ensuring that the data is valid and meets the specified criteria. It also adds a class serializer interceptor to transform the response objects into a plain JSON format. These changes improve the overall reliability and consistency of the application's data handling.

--- src/global-config.ts
+++ src/global-config.ts
@@ -1,8 +1,20 @@
 import { Reflector } from '@nestjs/core';
-import { ClassSerializerInterceptor, INestApplication } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
 import { WrapperDataInterceptor } from './shared/infrastructure/interceptors/wrapper-data/wrapper-data.interceptor';
 
 export function applyGlobalConfig(app: INestApplication) {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   app.useGlobalInterceptors(
     new WrapperDataInterceptor(),
     new ClassSerializerInterceptor(app.get(Reflector)),
--- src/users/infrastructure/dto/update-user.dto.ts
+++ src/users/infrastructure/dto/update-user.dto.ts
@@ -1,5 +1,8 @@
 import { UpdateUserUseCase } from '@/users/application/usecases/updateuser.usecase';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateUserDto implements Omit<UpdateUserUseCase.Input, 'id'> {
+  @IsString()
+  @IsNotEmpty()
   name: string;
 }
