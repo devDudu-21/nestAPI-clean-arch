b1e9bca5ecfaaa128da01c1adc2c2fddfbf36b3b
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-08-26 19:53:33 -0300

    feat: Refactor create.e2e-spec.ts to use data wrapper in API responses

--- src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
@@ -49,17 +49,12 @@ describe('Create method end-to-end tests', () => {
         .post('/users')
         .send(signupDto)
         .expect(201);
-      expect(Object.keys(res.body)).toStrictEqual([
-        'id',
-        'name',
-        'email',
-        'createdAt',
-      ]);
+      expect(Object.keys(res.body)).toStrictEqual(['data']);
 
-      const user = await repository.findById(res.body.id);
+      const user = await repository.findById(res.body.data.id);
       const presenter = UsersController.userToResponse(user.toJSON());
       const serialized = instanceToPlain(presenter);
-      expect(res.body).toStrictEqual(serialized);
+      expect(res.body.data).toStrictEqual(serialized);
     });
   });
 });
