94e38f6b05a2de50723902d1530b606ddc41d861
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-08-29 22:33:52 -0300

    feat: Add generateJwtToken and verifyJwt methods to AuthService
    This commit adds the `generateJwtToken` and `verifyJwt` methods to the `AuthService` class. These methods are used for generating and verifying JSON Web Tokens (JWT) for user authentication and authorization.

--- src/auth/infrastructure/auth.service.ts
+++ src/auth/infrastructure/auth.service.ts
@@ -1,4 +1,26 @@
+import { EnvConfigService } from '@/shared/infrastructure/env-config/env-config.service';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+type GeneratedJwtProps = {
+  accessToken: string;
+};
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: EnvConfigService,
+  ) {}
+
+  async generateJwtToken(userId: string): Promise<GeneratedJwtProps> {
+    const accessToken = await this.jwtService.signAsync({ id: userId }, {});
+    return { accessToken };
+  }
+
+  async verifyJwt(token: string) {
+    return this.jwtService.verifyAsync(token, {
+      secret: this.configService.getJwtSecret(),
+    });
+  }
+}
