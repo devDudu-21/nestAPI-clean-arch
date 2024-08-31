1c7b8473ef8465730b67164056abad453db309c2
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-08-31 15:37:36 -0300

    chore: Add Swagger documentation to DTOs

--- src/users/infrastructure/dto/list-users.dto.ts
+++ src/users/infrastructure/dto/list-users.dto.ts
@@ -1,20 +1,30 @@
 import { SortDirection } from '@/shared/domain/repositories/searchable-repository-contract';
 import { ListUsersUseCase } from '@/users/application/usecases/listusers.usecase';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional } from 'class-validator';
 
 export class ListUsersDto implements ListUsersUseCase.Input {
+  @ApiProperty({ description: 'Página que será retornada' })
   @IsOptional()
   page?: number;
 
+  @ApiProperty({ description: 'Quantidade de registros por página' })
   @IsOptional()
   perPage?: number;
 
+  @ApiProperty({
+    description: 'Coluna definida para ordenar os dados: "name" ou "createdAt"',
+  })
   @IsOptional()
   sort?: string;
 
+  @ApiProperty({
+    description: 'Ordenação dos dados: crescente (asc) ou descrescente (desc) ',
+  })
   @IsOptional()
   sortDir?: SortDirection;
 
+  @ApiProperty({ description: 'Dado informado para filtrar o resultado ' })
   @IsOptional()
   filter?: string;
 }
--- src/users/infrastructure/dto/update-password.dto.ts
+++ src/users/infrastructure/dto/update-password.dto.ts
@@ -1,13 +1,16 @@
 import { UpdatePasswordUseCase } from '@/users/application/usecases/updatepassword.usecase';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdatePasswordDto
   implements Omit<UpdatePasswordUseCase.Input, 'id'>
 {
+  @ApiProperty({ description: 'Senha nova do usuário' })
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({ description: 'Senha antiga do usuário' })
   @IsString()
   @IsNotEmpty()
   oldPassword: string;
