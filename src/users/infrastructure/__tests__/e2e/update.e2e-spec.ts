663bc6d95ec8de96a2aeef4ea5e2fd712a632c27
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-08-31 01:01:54 -0300

    feat: Refactor update.e2e-spec.ts to implements the authentication tests on method

--- src/users/infrastructure/__tests__/e2e/update.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/update.e2e-spec.ts
@@ -13,6 +13,8 @@ import { applyGlobalConfig } from '@/global-config';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
 import { UpdateUserDto } from '../../dto/update-user.dto';
+import { HashProvider } from '@/shared/application/providers/hash-provider';
+import { BcryptjsHashProvider } from '../../providers/hash-provider/bcryptjs-hash.provider';
 
 describe('UsersController Update method end-to-end tests', () => {
   let app: INestApplication;
@@ -21,6 +23,9 @@ describe('UsersController Update method end-to-end tests', () => {
   let updateUserDto: UpdateUserDto;
   const prismaService = new PrismaClient();
   let entity: UserEntity;
+  let hashProvider: HashProvider;
+  let hashPassword: string;
+  let accessToken: string;
 
   beforeAll(async () => {
     setupPrismaTests();
@@ -35,6 +40,8 @@ describe('UsersController Update method end-to-end tests', () => {
     applyGlobalConfig(app);
     await app.init();
     repository = module.get<UserRepository.Repository>('UserRepository');
+    hashProvider = new BcryptjsHashProvider();
+    hashPassword = await hashProvider.generateHash('fake_password');
   });
 
   beforeEach(async () => {
@@ -42,16 +49,24 @@ describe('UsersController Update method end-to-end tests', () => {
       name: 'test name',
     };
     await prismaService.user.deleteMany();
-    entity = new UserEntity(UserDataBuilder({}));
+    entity = new UserEntity(
+      UserDataBuilder({ email: 'a@a.com', password: hashPassword }),
+    );
     await repository.insert(entity);
+    const loginResponse = await request(app.getHttpServer())
+      .post('/users/login')
+      .send({ email: 'a@a.com', password: 'fake_password' })
+      .expect(200);
+    accessToken = loginResponse.body.accessToken;
   });
 
   describe('PUT /users/:id', () => {
-    it('should create an user', async () => {
+    it('should update a user', async () => {
       updateUserDto.name = 'new name';
       const res = await request(app.getHttpServer())
         .put(`/users/${entity._id}`)
         .send(updateUserDto)
+        .set('Authorization', `Bearer ${accessToken}`)
         .expect(200);
       const user = await repository.findById(entity._id);
       const presenter = UsersController.userToResponse(user.toJSON());
@@ -59,9 +74,37 @@ describe('UsersController Update method end-to-end tests', () => {
       expect(res.body.data).toStrictEqual(serialized);
     });
 
+    it('should throw unauthorized error with code 401 when the token is not provided', async () => {
+      updateUserDto.name = 'new name';
+      const res = await request(app.getHttpServer())
+        .put(`/users/${entity._id}`)
+        .send(updateUserDto);
+
+      expect(res.body).toStrictEqual({
+        statusCode: 401,
+        error: 'Unauthorized',
+        message: 'Token not found',
+      });
+    });
+
+    it('should throw unauthorized error with code 401 when the token is invalid', async () => {
+      updateUserDto.name = 'new name';
+      const res = await request(app.getHttpServer())
+        .put(`/users/${entity._id}`)
+        .send(updateUserDto)
+        .set('Authorization', `Bearer ${accessToken + 'fake'}`);
+
+      expect(res.body).toStrictEqual({
+        statusCode: 401,
+        error: 'Unauthorized',
+        message: 'Invalid token',
+      });
+    });
+
     it('should return an error with 422 code when the request body is invalid', async () => {
       const res = await request(app.getHttpServer())
         .put(`/users/${entity._id}`)
+        .set('Authorization', `Bearer ${accessToken}`)
         .send({})
         .expect(422);
 
@@ -77,6 +120,7 @@ describe('UsersController Update method end-to-end tests', () => {
       const res = await request(app.getHttpServer())
         .put(`/users/${entity._id}`)
         .send(updateUserDto)
+        .set('Authorization', `Bearer ${accessToken}`)
         .expect(422);
       expect(res.body.error).toBe('Unprocessable Entity');
       expect(res.body.message).toEqual([
@@ -89,6 +133,7 @@ describe('UsersController Update method end-to-end tests', () => {
       const res = await request(app.getHttpServer())
         .put(`/users/${entity._id}`)
         .send(Object.assign(updateUserDto, { xpto: 'xpto' }))
+        .set('Authorization', `Bearer ${accessToken}`)
         .expect(422);
       expect(res.body.message).toEqual(['property xpto should not exist']);
       expect(res.body.error).toBe('Unprocessable Entity');
@@ -98,6 +143,7 @@ describe('UsersController Update method end-to-end tests', () => {
       const res = await request(app.getHttpServer())
         .put(`/users/fakeId`)
         .send(updateUserDto)
+        .set('Authorization', `Bearer ${accessToken}`)
         .expect(404);
       expect(res.body.statusCode).toBe(404);
       expect(res.body.error).toBe('NotFound');
