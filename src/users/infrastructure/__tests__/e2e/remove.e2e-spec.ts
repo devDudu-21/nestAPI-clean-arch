f2f80871d740a270f906028dc71106e12e1e1e4f
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-08-30 23:37:36 -0300

    feat: Refactor remove.e2e-spec.ts to implements the authentication tests on method

--- src/users/infrastructure/__tests__/e2e/remove.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/remove.e2e-spec.ts
@@ -12,6 +12,8 @@ import { instanceToPlain } from 'class-transformer';
 import { applyGlobalConfig } from '@/global-config';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+import { HashProvider } from '@/shared/application/providers/hash-provider';
+import { BcryptjsHashProvider } from '../../providers/hash-provider/bcryptjs-hash.provider';
 
 describe('UsersController findOne method end-to-end tests', () => {
   let app: INestApplication;
@@ -19,6 +21,9 @@ describe('UsersController findOne method end-to-end tests', () => {
   let repository: UserRepository.Repository;
   const prismaService = new PrismaClient();
   let entity: UserEntity;
+  let hashProvider: HashProvider;
+  let hashPassword: string;
+  let accessToken: string;
 
   beforeAll(async () => {
     setupPrismaTests();
@@ -33,29 +38,64 @@ describe('UsersController findOne method end-to-end tests', () => {
     applyGlobalConfig(app);
     await app.init();
     repository = module.get<UserRepository.Repository>('UserRepository');
+    hashProvider = new BcryptjsHashProvider();
+    hashPassword = await hashProvider.generateHash('fake_password');
   });
 
   beforeEach(async () => {
     await prismaService.user.deleteMany();
-    entity = new UserEntity(UserDataBuilder({}));
+    entity = new UserEntity(
+      UserDataBuilder({ email: 'a@a.com', password: hashPassword }),
+    );
     await repository.insert(entity);
+    const loginResponse = await request(app.getHttpServer())
+      .post('/users/login')
+      .send({ email: 'a@a.com', password: 'fake_password' })
+      .expect(200);
+    accessToken = loginResponse.body.accessToken;
   });
 
   describe('DELETE /users/:id', () => {
     it('should remove an user', async () => {
       const res = await request(app.getHttpServer())
         .delete(`/users/${entity._id}`)
+        .set('Authorization', `Bearer ${accessToken}`)
         .expect(204);
       expect(res.body).toStrictEqual({});
     });
 
+    it('should throw unauthorized error with 401 code when the token is not provided', async () => {
+      const res = await request(app.getHttpServer()).delete(
+        `/users/${entity._id}`,
+      );
+      expect(res.body).toStrictEqual({
+        statusCode: 401,
+        error: 'Unauthorized',
+        message: 'Token not found',
+      });
+    });
+
+    it('should throw unauthorized error with 401 code when the token is invalid', async () => {
+      const res = await request(app.getHttpServer())
+        .delete(`/users/${entity._id}`)
+        .set('Authorization', `Bearer ${accessToken + 'fake'}`);
+      expect(res.body).toStrictEqual({
+        statusCode: 401,
+        error: 'Unauthorized',
+        message: 'Invalid token',
+      });
+    });
+
     it('should return an error with 404 code when the id is invalid', async () => {
       const res = await request(app.getHttpServer())
-        .get(`/users/fakeId`)
+        .get(`/users/${entity._id + 'fake'}`)
+        .set('Authorization', `Bearer ${accessToken}`)
         .expect(404);
       expect(res.body.statusCode).toBe(404);
       expect(res.body.error).toBe('NotFound');
-      expect(res.body.message).toBe('UserModel not found using ID fakeId');
+      expect(res.body.message).toBe(
+        `UserModel not found using ID ${entity._id + 'fake'}`,
+      );
     });
   });
 });
