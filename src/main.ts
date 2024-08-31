d3d36cadfc10da5b2ed246bcff6bdd1c90d678a8
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-08-31 15:24:27 -0300

    feat: Add Swagger documentation to the API
    This commit adds Swagger documentation to the API using the "@nestjs/swagger" package. Swagger is integrated with Fastify and provides a user-friendly interface to explore and test the API endpoints.

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,29 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { applyGlobalConfig } from './global-config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API')
+    .setDescription('The NestJS REST API Clean Architecture')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      description: 'Informar o JWT para autorizar o acesso',
+      name: 'Authorization',
+      scheme: 'bearer',
+      type: 'http',
+      in: 'header',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   applyGlobalConfig(app);
   await app.listen(3000, '0.0.0.0');
 }
